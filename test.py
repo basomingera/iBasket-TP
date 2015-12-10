65c4be278155b650eaf961f0f4b8c2aefd74a217
Rob <rob@rob-hp450.go.rwanda.cmu.local>
2015-12-10 10:57:46 +0200

    ADDED clouds class to graw and move the clouds

--- test.py
+++ test.py
@@ -32,3 +32,26 @@ def init(data):
 init(data)
 makeScreen=pygame.display.set_mode( (data.width, data.height))
 pygame.display.set_caption('iBasket Term Project')
+class Clouds(object):  # represents the cloud and move it
+    def __init__(self,x,y):
+        self.x1=x
+        self.x0=40
+        self.y=y
+        
+    def drawClouds(self,makeScreen):
+        #draw the clouds        
+        x1=self.x1
+        pygame.draw.circle(makeScreen, data.Cloud, 
+                           (x1+self.x0,(self.y)+self.x0), 3*self.x0//4)
+        pygame.draw.circle(makeScreen, data.Cloud, 
+                           (x1+(self.x0//2),(self.y)+3*self.x0//2), 
+                           3*self.x0//5)
+        pygame.draw.circle(makeScreen, data.Cloud, 
+                           (x1+(self.x0*3//2),(self.y)+3*self.x0//2), 
+                           3*self.x0//5)
+        
+    def moveCloud(self):
+        if((self.x1+(self.x0*3//2))>0):
+            self.x1-=1
+        else: self.x1=data.width
+        
