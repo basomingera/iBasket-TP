4c7944bc3576121021bd4d4cf7f9a09c327c0454
Rob <rob@rob-hp450.go.rwanda.cmu.local>
2015-12-10 10:54:51 +0200

    ADDED creation and initialization of data struct to store all global variables

--- test.py
+++ test.py
@@ -13,3 +13,11 @@ import math
 #all pygame modules declared are initialized
 pygame.init()
 
+class Struct(object): pass
+data = Struct()
+
+def init(data):
+    data.height=700
+    data.width=900
+    data.level=1
+init(data)
